8ff63af6a2d14dcbc1e799152aa26fb0823cc51e
Ashish-700 <ashishberiya14@gmail.com>
2025-10-04 02:52:57 +0530

    using Online dependencies to use prefined feature and page's

---
+++ Ashish_Spring_Secure/src/main/java/Ashish_Spring_Secure/AshishSpringSecureApplication.java
@@ -0,0 +1,13 @@
+package Ashish_Spring_Secure;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AshishSpringSecureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AshishSpringSecureApplication.class, args);
+	}
+
+}
